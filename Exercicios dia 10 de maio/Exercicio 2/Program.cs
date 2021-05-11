86ebe8a408a0f711804ce82bf3e9b71676f00b07
Gustavo-Henrique-Ferreira-Alves <gustavohferreiraalves@gmail.com>
2021-05-11 09:05:17 -0300

    Finalizado

--- Exercicios dia 10 de maio/Exercicio 2/Program.cs
+++ Exercicios dia 10 de maio/Exercicio 2/Program.cs
@@ -6,47 +6,51 @@ namespace Exercicio_2
     {
         static void Main(string[] args)
         {
-        double Desconto = 0;
-        double Gasolina = 5.30;
-        double Alcool = 4.90;
-        double Total = 0;
+            double Gasolina = 5.30;
+            double Alcool = 4.90;
+            double Desconto = 0;
+            double Total = 0;
 
-        
-        Console.WriteLine("Digite o tipo de combustível, sendo G para gasolina e A para álcool (Não esqueça de digitar em maiúsculo): ");
-        string Combustivel = (Console.ReadLine());
-            
-        Console.WriteLine("Digite a quantidade de litros (Apenas o número): ");
-        int Litros = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Digite o tipo de combustível, sendo G para gasolina e A para álcool (Não esqueça de digitar em maiúsculo): ");
+            string Combustivel = (Console.ReadLine());
 
-        switch (Combustivel){
-            case "G":
+            Console.WriteLine("Digite a quantidade de litros (Apenas o número): ");
+            int Litros = int.Parse(Console.ReadLine());
 
-                if (Litros <= 20){
-                    Desconto = 5.30 * 0.04;
-                    Total = Litros * (Gasolina - Desconto);
-                }
+            switch (Combustivel)
+            {
+                case "G":
 
-                else{
-                    Desconto = 5.30 * 0.06;
-                    Total = Litros * (Gasolina - Desconto); 
-                }
-                break;
+                    if (Litros <= 20)
+                    {
+                        Desconto = 5.30 * 0.04;
+                        Total = Litros * (Gasolina - Desconto);
+                    }
 
-            case "A":
-                if (Litros <= 20){
-                    Desconto = 4.90 * 0.03;
-                    Total = Litros * (Alcool - Desconto);
-                }
+                    else
+                    {
+                        Desconto = 5.30 * 0.06;
+                        Total = Litros * (Gasolina - Desconto);
+                    }
+                    break;
 
-                else{
-                    Desconto = 4.90 * 0.05;
-                    Total = Litros * (Alcool - Desconto);
-                }
-                break;
-                
+                case "A":
+                    if (Litros <= 20)
+                    {
+                        Desconto = 4.90 * 0.03;
+                        Total = Litros * (Alcool - Desconto);
+                    }
+
+                    else
+                    {
+                        Desconto = 4.90 * 0.05;
+                        Total = Litros * (Alcool - Desconto);
+                    }
+                    break;
+
                 default:
                     Console.WriteLine("Inválido!!! Por Favor siga as instruções passadas");
-                break;
+                    break;
             }
 
             Console.WriteLine("O total a ser pago é igual a R$ " + Total);
