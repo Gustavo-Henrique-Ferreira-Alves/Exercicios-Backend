234c363a155304155cd077e923a60e24fa315937
Gustavo-Henrique-Ferreira-Alves <gustavohferreiraalves@gmail.com>
2021-05-14 21:57:46 -0300

    Exercicio finalizado

---
+++ Projeto Passagens Aéreas/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Projeto_Passagens_Aéreas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Usuario;
+            string Senha;
+            string SenhaCorreta;
+            string Opcoes;
+            string[] NomePassageiro = new string[5];
+            string[] Origem = new string[5];
+            string[] Destino = new string[5];
+            string[] DataVoo = new string[5];
+            int i = 0;
+            string Resposta;
+            bool Condicao = false;
+
+            do
+            {
+                Console.WriteLine("Por favor digite o nome de usuário: ");
+                Usuario = Console.ReadLine();
+
+                Console.WriteLine("Digite a sua senha para acessar o menu: ");
+                Senha = (Console.ReadLine());
+
+                SenhaCorreta = "123456";
+
+                if (Senha == SenhaCorreta)
+                {
+                    do
+                    {
+                        Console.WriteLine(@" 
+                |==================================================|
+                |                                                  |
+                |           SELECIONE UMA DAS OPÇÕES               |                               
+                |                                                  |     
+                |           1- Cadastrar                           |
+                |           2- Listar Passagens                    |
+                |           0- Sair                                |
+                |                                                  |
+                |==================================================|
+                ");
+                        Opcoes = Console.ReadLine();
+
+                        switch (Opcoes)
+                        {
+                            case "1":
+                                do
+                                {
+                                    Console.WriteLine("Por favor digite o nome do passageiro: ");
+                                    NomePassageiro[i] = Console.ReadLine();
+
+                                    Console.WriteLine("Digite onde você mora: ");
+                                    Origem[i] = Console.ReadLine();
+
+                                    Console.WriteLine("Digite para onde você gostaria de ir: ");
+                                    Destino[i] = Console.ReadLine();
+
+                                    Console.WriteLine("Digite a data do voo: ");
+                                    DataVoo[i] = Console.ReadLine();
+
+                                    Console.WriteLine("Você quer digitar outra passagem (Digite sim ou não): ");
+                                    Resposta = Console.ReadLine();
+                                    i++;
+                                } while (Resposta == "sim");
+
+                                break;
+
+                            case "2":
+                                Console.Clear();
+                                for (var c = 0; c < i; c++)
+                                {
+                                    Console.WriteLine($"\n Nome = {NomePassageiro[c]}       Origem = {Origem[c]}      Destino = {Destino[c]}       Data do Voo = {DataVoo[c]}\n");
+                                }
+                                break;
+
+                            case "0":
+                                Console.WriteLine("Obrigado por usar nosso sistema");
+                                Condicao = true;
+                                break;
+
+                            default:
+
+                                break;
+                        }
+                    } while (!Condicao);
+                }
+                else
+                {
+                    Console.WriteLine("\nVocê não conseguiu entrar no menu!!! Verifique se o nome de usuário e senha estão corretos\n");
+                }
+
+            } while (Senha != SenhaCorreta);
+        }
+    }
+}
---
+++ Sistema de produtos/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Sistema_de_produtos
+{
+    class Program
+    {
+        static string[] Nome = new string[10];
+        static float[] Preco = new float[10];
+        static bool[] Promocao = new bool[10];
+        static int i = 0;
+        static string Opcoes;
+        static string RespostaPromocao;
+        static string RespostaProduto;
+        static string SimOuNao;
+        static bool Condicao = false;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem Vindo ao sistema!!!!!");
+            do
+            {
+                Menu();
+                Opcoes = Console.ReadLine();
+
+                switch (Opcoes)
+                {
+                    case "1":
+                        do
+                        {
+                            Cadastrar();
+                        } while (RespostaProduto == "sim");
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        Listar();
+                        break;
+
+                    case "0":
+                        Console.WriteLine("Obrigado por usar nosso sistema");
+                        Condicao = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Erro!!! Por favor digitei apenas o número da função que você quer usar: ");
+                        break;
+                }
+            } while (!Condicao);
+
+
+            static void Menu()
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(@" 
+                |==================================================|
+                |                                                  |
+                |           SELECIONE UMA DAS OPÇÕES               |                               
+                |                                                  |     
+                |           1- Cadastrar Produtos                  |
+                |           2- Listar Produtos                     |
+                |           0- Sair                                |
+                |                                                  |
+                |==================================================|
+                ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            static void Cadastrar()
+            {
+                Console.WriteLine("Por favor digite o nome do produto (Você pode cadastrar no máximo 10): ");
+                Nome[i] = Console.ReadLine();
+
+                Console.WriteLine("Digite o preço unitário do produto (Digite apenas o valor, sem R$ ou reais): ");
+                Preco[i] = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite se o produto está em promoção: ");
+                RespostaPromocao = Console.ReadLine().ToLower();
+
+                if (RespostaPromocao == "sim")
+                {
+                    Promocao[i] = true;
+                }
+                else
+                {
+                    Promocao[i] = false;
+                }
+
+                Console.WriteLine("Você quer digitar outro produto (Digite apenas sim ou não): ");
+                RespostaProduto = Console.ReadLine().ToLower();
+                i++;
+            }
+
+            static void Listar()
+            {
+                for (var c = 0; c < i; c++)
+                {
+                    if (Promocao[c] == true)
+                    {
+                        SimOuNao = Promocao[c].ToString();
+                        SimOuNao = "Sim";
+                    }
+                    else
+                    {
+                        SimOuNao = Promocao[c].ToString();
+                        SimOuNao = "Não";
+                    }
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($@" 
+                         
+                Nome = {Nome[c]}                     
+                Preço = R${Preco[c]} 
+                Promoção = {SimOuNao}            
+                ");
+                }
+            }
+        }
+    }
+}
